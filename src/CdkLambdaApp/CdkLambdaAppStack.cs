cbec6a61480b27ad2d74fc3f72950dff28c25b98
Mickey Shukla <>
2021-04-27 18:56:31 -0400

    Initial commit

---
+++ src/CdkLambdaApp/CdkLambdaAppStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace CdkLambdaApp
+{
+    public class CdkLambdaAppStack : Stack
+    {
+        internal CdkLambdaAppStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
